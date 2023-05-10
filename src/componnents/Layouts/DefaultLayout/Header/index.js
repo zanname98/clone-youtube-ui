dc2b6bcf51529e60cf564c9238e841c3931a8eed
minhtam <zanname98@gmail.com>
2023-05-10 15:39:15 +0700

    tippy for search

--- src/componnents/Layouts/DefaultLayout/Header/index.js
+++ src/componnents/Layouts/DefaultLayout/Header/index.js
@@ -1,4 +1,4 @@
-import Tippy from '@tippyjs/react';
+import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -14,6 +14,7 @@ import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Button from '~/componnents/Button';
+import { Wrapper as PopperWrapper } from '~/componnents/Popper';
 
 const cx = classNames.bind(styles);
 
@@ -25,28 +26,38 @@ function Header() {
                 <img className={cx('logo-icon')} src={images.logo} alt="logo" />
             </div>
             <div className={cx('search')}>
-                <div className={cx('form')}>
-                    <input type="text" placeholder="Tim Kiem" />
-                    <div className={cx('btn')}>
-                        <button className={cx('keyboard')}>
-                            <FontAwesomeIcon icon={faKeyboard} />
-                        </button>
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faClose} />
+                <Tippy
+                    // visible
+                    placement="top-end"
+                    render={(attrs) => (
+                        <div className={cx('result-search')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h3>d</h3>
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('form')}>
+                        <input type="text" placeholder="Tim Kiem" />
+                        <div className={cx('btn')}>
+                            <button className={cx('keyboard')}>
+                                <FontAwesomeIcon icon={faKeyboard} />
+                            </button>
+                            <button className={cx('clear')}>
+                                <FontAwesomeIcon icon={faClose} />
+                            </button>
+                        </div>
+                        <button className={cx('search')}>
+                            <FontAwesomeIcon icon={faSearch} />
                         </button>
                     </div>
-                    <button className={cx('search')}>
-                        <FontAwesomeIcon icon={faSearch} />
-                    </button>
-                </div>
+                </Tippy>
                 <button className={cx('micro')}>
                     <FontAwesomeIcon icon={faMicrophone} />
                 </button>
             </div>
             <div className={cx('action')}>
-                <Tippy content="Cai dat">
-                    <FontAwesomeIcon className={cx('topRight-menu')} icon={faEllipsisVertical} />
-                </Tippy>
+                <FontAwesomeIcon className={cx('topRight-menu')} icon={faEllipsisVertical} />
                 <Button href="/login" target="_blank" blue leftIcon onClick={() => alert('clicked')}>
                     Dang nhap
                 </Button>
